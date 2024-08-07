5d735032fc2386fa7a9673b88773ab0b15050ccb
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-08-07 08:14:49 -0600

    COMPLETAR LA FUCION DE MASTER CON MAIN

---
+++ src/main/java/control/asistencia/QRCheck/controlers/HomeController.java
@@ -0,0 +1,15 @@
+package control.asistencia.QRCheck.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String Index(){
+        return "home/index";
+    }
+}
