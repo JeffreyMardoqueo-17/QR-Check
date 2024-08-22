12d44aca17e7f441bcac6d9f57ca5bc09888d15c
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-08-22 07:48:55 -0600

    Usuario :sparkles:

---
+++ src/main/java/control/asistencia/QRCheck/controlers/UsuarioController.java
@@ -0,0 +1,2 @@
+package control.asistencia.QRCheck.controlers;public class UsuarioController {
+}
--- src/main/java/control/asistencia/QRCheck/services/iterfaces/IUsuarioService.java
+++ src/main/java/control/asistencia/QRCheck/services/iterfaces/IUsuarioService.java
@@ -1,21 +1,2 @@
-package control.asistencia.QRCheck.services.iterfaces;
-
-import control.asistencia.QRCheck.models.Empresa;
-import control.asistencia.QRCheck.models.Usuario;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface IUsuarioService {
-    Page<Usuario> buscarTodosPaginados(Pageable pageable);
-
-    List<Usuario> obtenerTodos();
-
-    Optional<Usuario> buscarPorId(Integer id);
-
-    Usuario crearOEditar(Usuario usuario);
-
-    void eliminarPorId(Integer id);
+package control.asistencia.QRCheck.services.iterfaces;public interface IUsuarioService {
 }
