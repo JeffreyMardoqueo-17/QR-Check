57c1d4a951f1627d7fd0c4ada441e07c2e7e8732
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-08-22 07:58:38 -0600

    Servico Usuario :sparkles:

--- src/main/java/control/asistencia/QRCheck/services/implementations/UsuarioService.java
+++ src/main/java/control/asistencia/QRCheck/services/implementations/UsuarioService.java
@@ -1,4 +1,43 @@
 package control.asistencia.QRCheck.services.implementations;
 
-public class UsuarioService {
+import control.asistencia.QRCheck.models.Usuario;
+import control.asistencia.QRCheck.repositories.IUsuarioRepository;
+import control.asistencia.QRCheck.services.iterfaces.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UsuarioService implements IUsuarioService {
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Override
+    public Page<Usuario> buscarTodosPaginados(Pageable pageable) {
+        return usuarioRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Usuario> obtenerTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorId(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    @Override
+    public Usuario createOrEditOne(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+
 }
